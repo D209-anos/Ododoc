42bfb3c3b59d0e60da977a9be2747cfdfc996478
이주미 <mikj5@naver.com>
2024-04-26 09:33:28 +0900

    fix: directory edit

--- frontend/fe_repo/src/App.tsx
+++ frontend/fe_repo/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   return (
       <Router>
         <div>
-          <Menu/>
+          <Menu />
           <Routes>
             <Route path='/' element={<HomePage/>}/>
             <Route path='/start' element={<Start/>}/>
