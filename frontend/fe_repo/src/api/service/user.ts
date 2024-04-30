254639d9c0f04cdd28f82d6af23d11debec250b4
이주미 <mikj5@naver.com>
2024-04-30 14:52:13 +0900

    feat: 로그아웃 기능 구현

--- frontend/fe_repo/src/api/service/user.ts
+++ frontend/fe_repo/src/api/service/user.ts
@@ -17,3 +17,25 @@ export const sendCodeToBackend = async(code: string, provider: string, setAccess
         console.error('Error sending authorization code:', error)
     }
 }
+
+// 로그아웃
+export const logout = async() => {
+    try {
+        const accessToken = localStorage.getItem('accessToken');
+        if (!accessToken) {
+            throw new Error('엑세스 토큰을 찾을 수 없네욤..');
+        }
+
+        const response = await api.post('/oauth2/logout', {}, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
+        console.log('성공적으로 로그인 되었음!', response.data);
+
+        // 로그아웃 후 local storage 토큰 제거
+        localStorage.removeItem('accessToken');
+    } catch (error) {
+        console.error('로그아웃 도중 문제 발생!!!', error);
+    }
+}
