a8d6191e9d35ebefa3504f5f8e308f4284681418
이주미 <mikj5@naver.com>
2024-05-01 16:30:09 +0900

    feat: 홈 페이지 (2) 수정

--- frontend/fe_repo/src/components/homePage/HomePage2.tsx
+++ frontend/fe_repo/src/components/homePage/HomePage2.tsx
@@ -2,7 +2,7 @@ import { useRef } from 'react';
 import { useIntersectionObserver } from '../../hooks/useIntersectionObserver';
 import VisualScreen from '../../assets/images/homePageImage/visualScreen.png'
 import IntellijScreen from '../../assets/images/homePageImage/intellijScreen.png'
-import EditorScreen from '../../assets/images/editPageImage/editorpage.png'
+import EditorScreen from '../../assets/images/homePageImage/editorScreen.png'
 import Home2 from '../../css/components/homePage/Home2.module.css'
 
 function HomePage2() {
@@ -28,9 +28,12 @@ function HomePage2() {
                 <p className={Home2.bmjuaFont}>빌드 감지, 문서 정리를<br/> 자동으로</p>
             </div>
             <div className={`${Home2.halfContainerSide} ${Home2.imageSide}`}>
-                <img src={VisualScreen} alt="VisualScreen" className={Home2.visualImage} />
-                <img src={IntellijScreen} alt="IntellijScreen" className={Home2.intellijImage} />
-                <img src={EditorScreen} alt="EditorScreen" className={Home2.editorImage} />
+                <div style={{ backgroundImage: `url(${VisualScreen})` }} className={Home2.visualImage} />
+                <div style={{ backgroundImage: `url(${IntellijScreen})` }} className={Home2.intellijImage} />
+                <div>
+                    <div style={{ backgroundImage: `url(${EditorScreen})` }} className={Home2.editorImage} />
+                    <p>IntelliJ와 Visual Studio Code에서 빌드만 하면, 깔끔히 정리된 문서를 제공해 드려요.</p>
+                </div>
             </div>
         </section>
     )
