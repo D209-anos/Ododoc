ef5c178c00849557a80b1ae284e46e1dcb4a8a0d
이준희 <non_duality@naver.com>
2024-05-07 15:45:08 +0900

    feat: 블럭으로 가공된 내용을 메인 서버로 전송

--- backend/process/src/main/java/com/ssafy/ododoc/process/service/DataTypeHandlerService.java
+++ backend/process/src/main/java/com/ssafy/ododoc/process/service/DataTypeHandlerService.java
@@ -3,58 +3,39 @@ package com.ssafy.ododoc.process.service;
 import com.ssafy.ododoc.process.dto.receive.MessageDto;
 import com.ssafy.ododoc.process.type.DataType;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class DataTypeHandlerService {
 
     private final DataTransferService dataTransferService;
 
-    public void handle(MessageDto messageDto){
-
+    public void handle(MessageDto messageDto) {
         DataType dataType = messageDto.getDataType();
+        log.info("Data Type : {}", dataType);
+        log.info("Message Content : {}", messageDto.getContent());
+
         switch (dataType){
             case SIGNAL:
-                System.out.println(dataType);
-                System.out.println(messageDto.getContent());
                 break;
 
             case OUTPUT:
-                System.out.println(dataType);
-                System.out.println(messageDto.getContent());
                 break;
 
             case ERROR:
-                System.out.println(dataType);
-                System.out.println(messageDto.getContent());
+                dataTransferService.transferDataForSave(messageDto);
                 break;
 
             case SCM:
-                System.out.println(dataType);
-                System.out.println(messageDto.getContent());
                 break;
 
             default:
-                System.out.println(dataType);
+                log.info("잘못된 데이터 타입");
                 break;
         }
-    }
-
-    private void processSignal(MessageDto messageDto){
-        System.out.println(messageDto.toString());
-    }
-
-    private void processOutput(MessageDto messageDto) {
-        dataTransferService.transferDataForSave(messageDto);
-    }
-
-    private void processError(MessageDto messageDto){
-        dataTransferService.transferDataForSave(messageDto);
-    }
-
-    private void processSCM(MessageDto messageDto) {
-        System.out.println(messageDto.toString());
     }
 
 }
