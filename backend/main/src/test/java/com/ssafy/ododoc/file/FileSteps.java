86cd72a2926635b4eed16d15161d4139dcc33b6a
kimjuyi <juyi077@naver.com>
2024-05-12 23:03:55 +0900

    refactor: 변경된 에디터 형식에 맞추어 dto 변경 및 그에 따른 테스트 코드 수정

--- backend/main/src/test/java/com/ssafy/ododoc/file/FileSteps.java
+++ backend/main/src/test/java/com/ssafy/ododoc/file/FileSteps.java
@@ -1,9 +1,6 @@
 package com.ssafy.ododoc.file;
 
-import com.ssafy.ododoc.file.dto.Block;
-import com.ssafy.ododoc.file.dto.Content;
-import com.ssafy.ododoc.file.dto.Props;
-import com.ssafy.ododoc.file.dto.Styles;
+import com.ssafy.ododoc.file.dto.*;
 import com.ssafy.ododoc.file.dto.request.FileRequest;
 import org.springframework.stereotype.Component;
 
@@ -13,36 +10,36 @@ import java.util.List;
 @Component
 public class FileSteps {
 
-    private final String blockId = "c329b660-69e3-47f5-a296-6d123d42aeab";
+    private final String blockId = "a10a4656-4ca4-4271-86cc-3cb1351ed3a6";
+    private final String valueId = "ae39c537-dba2-4192-8b40-cf8ab8c84a46";
     private final String type = "paragraph";
-    private Props props = Props.builder().
-            textColor("default")
-            .backgroundColor("default")
-            .textAlignment("left")
-            .build();
 
-    private Styles styles = Styles.builder()
-            .bold(false)
-            .italic(false)
-            .underline(false)
-            .strike(false)
-            .code(false)
+    private Props props = Props.builder()
+            .nodeType("block")
             .build();
 
     private List<Content> content = List.of(Content.builder()
-            .type("text")
-            .text("테스트")
-            .styles(styles)
+            .text("파일 내용 테스트")
             .build());
 
-    private Block block = Block.builder()
-            .id(blockId)
+    private List<Value> value = List.of(Value.builder()
+            .id(valueId)
             .type(type)
+            .children(content)
             .props(props)
-            .content(content)
+            .build());
+
+    private Meta meta = Meta.builder()
+            .order(0)
+            .depth(0)
             .build();
 
-    private List<Block> contentList = List.of(block);
+    private List<Block> contentList = List.of(Block.builder()
+            .id(blockId)
+            .value(value)
+            .type(type)
+            .meta(meta)
+            .build());
 
     public FileRequest 저장파일_생성(Long directoryId) {
         return FileRequest.builder()
