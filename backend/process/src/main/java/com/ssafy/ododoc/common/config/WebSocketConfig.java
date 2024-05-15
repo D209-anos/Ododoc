74ebd4278914c420d034fb1481247ae676056063
이준희 <non_duality@naver.com>
2024-05-16 08:03:27 +0900

    feat : IntelliJ 파일 저장 기능 구현

--- backend/process/src/main/java/com/ssafy/ododoc/common/config/WebSocketConfig.java
+++ backend/process/src/main/java/com/ssafy/ododoc/common/config/WebSocketConfig.java
@@ -2,10 +2,12 @@ package com.ssafy.ododoc.common.config;
 
 import com.ssafy.ododoc.common.handler.SocketHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket
@@ -15,6 +17,15 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(socketHandler, "/ws").setAllowedOrigins("*");
+        registry.addHandler(socketHandler, "/ws")
+                .setAllowedOrigins("*");
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(8 * 1024 * 1024); // 8MB
+        container.setMaxBinaryMessageBufferSize(8 * 1024 * 1024); // 8MB
+        return container;
     }
 }
