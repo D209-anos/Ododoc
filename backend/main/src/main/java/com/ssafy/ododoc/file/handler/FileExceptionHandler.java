71b45d40b58f4573d05bf875a09faec05f0dd551
kimjuyi <juyi077@naver.com>
2024-05-16 17:09:25 +0900

    refactor: 플러그인에서 파일 내용 추가 요청 시 redis에 저장하도록 로직 수정 및 크롬 익스텐션의 keyword type 추가

--- backend/main/src/main/java/com/ssafy/ododoc/file/handler/FileExceptionHandler.java
+++ backend/main/src/main/java/com/ssafy/ododoc/file/handler/FileExceptionHandler.java
@@ -3,7 +3,6 @@ package com.ssafy.ododoc.file.handler;
 import com.ssafy.ododoc.common.type.ErrorResponse;
 import com.ssafy.ododoc.file.exception.FileBadRequestException;
 import com.ssafy.ododoc.file.exception.NotAllowedImageException;
-import com.ssafy.ododoc.file.exception.VisitCountNotNullException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -22,10 +21,5 @@ public class FileExceptionHandler {
     @ExceptionHandler(FileBadRequestException.class)
     public List<ErrorResponse> fileBadRequestExceptionHandler(FileBadRequestException e) {
         return makeErrorResponse(e, "file");
-    }
-
-    @ExceptionHandler(VisitCountNotNullException.class)
-    public List<ErrorResponse> visitCountNotNullExceptionHandler(VisitCountNotNullException e) {
-        return makeErrorResponse(e, "visitCount");
     }
 }
