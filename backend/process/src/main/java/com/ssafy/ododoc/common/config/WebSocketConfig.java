e3ba1c550e3ecca0cfe2c2d27112140fe8f38ea6
윤정영 <dbswjddud100@naver.com>
2024-05-16 16:02:23 +0900

    🚑 Hotfix: Jenkinsfile 수정 5

--- backend/process/src/main/java/com/ssafy/ododoc/common/config/WebSocketConfig.java
+++ backend/process/src/main/java/com/ssafy/ododoc/common/config/WebSocketConfig.java
@@ -15,6 +15,6 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(socketHandler, "/ws").setAllowedOrigins("*");
+        registry.addHandler(socketHandler, "/process/ws").setAllowedOrigins("*");
     }
 }
