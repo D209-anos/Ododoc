915a170efe1a4fc8d2696ed6b54e5a8db28e7881
kimjuyi <juyi077@naver.com>
2024-05-16 10:02:01 +0900

    refactor: Springboot가 켜질 때 Redis에 MongoDB 데이터를 저장하는 로직 추가

--- backend/main/src/main/java/com/ssafy/ododoc/file/entity/RedisFile.java
+++ backend/main/src/main/java/com/ssafy/ododoc/file/entity/RedisFile.java
@@ -7,16 +7,18 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
+import java.io.Serializable;
 import java.util.LinkedHashMap;
 
 @Builder
 @Getter
 @Setter
 @RedisHash(value = "file")
-public class RedisFile {
+public class RedisFile implements Serializable {
 
     @Id
     private Long id;
 
+    private Integer lastOrder;
     private LinkedHashMap<String, Block> content;
 }
